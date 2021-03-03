f0200b5b5fa26e4b413f1bed4c827432a1604c55
EternalShallow <eternalShallow@163.com>
2021-03-04 00:32:14 +0800

    页面基本画完，明天替换图标及echart图标绘制

--- pages/reward/index.vue
+++ pages/reward/index.vue
@@ -6,7 +6,8 @@
       <div class="balance-list display-flex box-center-Y">
         <div class="balance-item">
           <div class="title">Your Transferrable <br>
-               GlC Holdings</div>
+                             GlC Holdings
+          </div>
           <div class="amount">3345</div>
         </div>
         <div class="balance-item">
@@ -36,7 +37,219 @@
       </div>
       <div class="con-box box-flex1">
         <div class="interface-tab display-flex box-center-Y">
-          <div class="interface-tab-item no-select" @click="changeInterfaceTab(i)" :class="{active: interfaceList.index === i}" v-for="(v, i) in interfaceList.list" :key="`interface-list-${i}`" v-html="v"></div>
+          <div class="interface-tab-item no-select" @click="changeInterfaceTab(i)"
+               :class="{active: interfaceTab.index === i}" v-for="(v, i) in interfaceTab.list"
+               :key="`interface-list-${i}`" v-html="v.name"></div>
+        </div>
+        <div v-show="interfaceTab.list[interfaceTab.index].type === 'ending'">
+          <div class="item-title">LOCK WRITE (LP) TOKENS AND EARN HEGIC REWARDS</div>
+          <div class="item-desc">Liquidity providers can claim rewards in HEGIC After providing WBTC or ETH to one of
+                                 the pools you will receive writeWBTC or writeETH tokens. You will need to lock these
+                                 write tokens to start receiving rewards in HEGIC tokens After locking them your share
+                                 in rewards will be increasing each block. You will need to manually claim HEIC.
+          </div>
+          <div class="end-tab display-flex box-center-Y">
+            <div class="end-item no-select" @click="changeEndTab(i)" :class="{active: endTab.index === i}"
+                 v-for="(v, i) in endTab.list" :key="`endTab-list-${i}`" v-html="v.currency"></div>
+          </div>
+          <div class="total-box display-flex box-center-Y">
+            <div class="total-title">Total Provided:</div>
+            <div class="emColor">14.780.276</div>
+            <div class="currency-text">writeETH</div>
+          </div>
+          <div class="display-flex box-center-Y total-list">
+            <div class="total-item" v-for="(v, i) in endTotalList" :key="`total-list${i}`">
+              <div class="icon-img"><img :src="v.icon_url" alt=""></div>
+              <div class="number-box display-flex box-center-end">
+                <div>{{v.balance}}</div>
+                <div class="currency-b">{{v.currency}}</div>
+              </div>
+              <div class="line"></div>
+              <div class="title">{{v.title}}</div>
+              <div class="sub-title">{{v.sub_title}}</div>
+              <div class="btn-total no-select">{{v.btn_text}}</div>
+            </div>
+          </div>
+        </div>
+        <div v-show="interfaceTab.list[interfaceTab.index].type === 'utilization'">
+          <div class="item-title">TRADE WBTC ETH OPTIONS AND EARN HEGIC REWARDS</div>
+          <div class="item-desc">Options buyers can claim rewitrdls ir HEGIC After buying ETH or WBTCoptions an Hegie
+                                 you will be able to clalm your rewards Rewards can be claimed for each option. Rewards
+                                 size in HEdIC tokens depends on the size-and period of an option.You will need to
+                                 imiumiaMly elaine HEnIC.
+          </div>
+          <div class="end-tab display-flex box-center-Y">
+            <div class="end-item no-select" @click="changeUtilizationTab(i)"
+                 :class="{active: utilizationTab.index === i}" v-for="(v, i) in utilizationTab.list"
+                 :key="`utilization-list-${i}`" v-html="v.currency"></div>
+          </div>
+          <div class="utilization-data">
+            <div class="utilization-item header display-flex box-center-Y">
+              <div class="box-flex1">ID</div>
+              <div class="box-flex1">Type</div>
+              <div class="box-flex1">Amount</div>
+              <div class="box-flex1">Fee</div>
+              <div class="box-flex1">Reward</div>
+              <div class="box-flex1">Claim</div>
+            </div>
+            <div class="utilization-item display-flex box-center-Y" v-for="(v, i) in utilizationData"
+                 :key="`utilizationData-${i}`">
+              <div class="box-flex1">{{v.id}}</div>
+              <div class="box-flex1">{{v.type}}</div>
+              <div class="box-flex1">{{v.amount}}</div>
+              <div class="box-flex1">{{v.fee}}</div>
+              <div class="box-flex1">{{v.reward}}</div>
+              <div class="box-flex1">{{v.claim}}</div>
+            </div>
+          </div>
+        </div>
+        <div v-show="interfaceTab.list[interfaceTab.index].type === 'stakeEarn'">
+          <div class="item-title">HOLD STAKING LOTS AND EARN WBTC OR ETH</div>
+          <div class="item-desc">You need 888,000 HEGIC for a staking lot. ou can buy a staking lot by converting
+                                 888,000 HEGIC into a staking lot that you will be able to stake for receiving a share
+                                 of staking rewards distributed among lots holder.One lot can be used to receive rewards
+                                 in WBTC or ETH Current rewards size: 1% of each options size.
+          </div>
+          <div class="end-tab display-flex box-center-Y">
+            <div class="end-item no-select" @click="changeEndTab(i)" :class="{active: endTab.index === i}"
+                 v-for="(v, i) in endTab.list" :key="`endTab-list-${i}`" v-html="v.currency"></div>
+          </div>
+          <div class="total-box display-flex box-center-Y">
+            <div class="total-title">Total Provided:</div>
+            <div class="emColor">14.780.276</div>
+            <div class="currency-text">writeETH</div>
+          </div>
+          <div class="display-flex box-center-Y total-list">
+            <div class="total-item stake-earn" v-for="(v, i) in stakeEarnList" :key="`stake-list${i}`">
+              <div class="icon-img"><img :src="v.icon_url" alt=""></div>
+              <div class="number-box display-flex box-center-end">
+                <div>{{v.balance}}</div>
+                <div class="currency-b">{{v.currency}}</div>
+              </div>
+              <div class="line"></div>
+              <div class="title">{{v.title}}</div>
+              <div class="sub-title">{{v.sub_title}}</div>
+              <div class="btn-total no-select">{{v.btn_text}}</div>
+            </div>
+          </div>
+        </div>
+        <div v-show="interfaceTab.list[interfaceTab.index].type === 'bonding'">
+          <div class="item-title">BONDING CURVE CONTRACT:BUY OR SELL HEGIC TOKENS</div>
+          <div class="end-tab display-flex box-center-Y">
+            <div class="end-item no-select" @click="changeBoundTab(i)" :class="{active: boundTab.index === i}"
+                 v-for="(v, i) in boundTab.list" :key="`endTab-list-${i}`" v-html="v"></div>
+          </div>
+          <div class="bound-info display-flex box-center-Y">
+            <div class="box-flex1">
+              <div class="title">Your Buy Price</div>
+              <div class="number-box display-flex box-center-end">
+                <div>35465</div>
+                <div class="currency-b">ETH</div>
+              </div>
+            </div>
+            <div class="box-flex1">
+              <div class="title">Acquired on the Bonding Curve</div>
+              <div class="number-box display-flex box-center-end">
+                <div>122131</div>
+                <div class="currency-b">HEGIC</div>
+              </div>
+            </div>
+            <div class="box-flex1">
+              <div class="title">Your Buy Price</div>
+              <div class="number-box display-flex box-center-end">
+                <div>122131</div>
+                <div class="currency-b">HEGIC</div>
+              </div>
+            </div>
+          </div>
+          <div class="title-curw">HEGIC Bonding Curw</div>
+          <div class="liquidity-main display-flex box-center-start">
+            <div class="liquidity-text-box">
+              <div class="liquidity-text-item">
+                <div class="display-flex box-center-Y">
+                  <div class="icon-img"><img src="../../assets/image/logo@2x.png" alt=""></div>
+                  <div>Problem</div>
+                </div>
+                <div class="text-desc">Writers(Liquidity providers)receive writeWBTC tokens(ERC20)that give them a share in the poots premiums that are distributed to them after the options contracts</div>
+              </div>
+              <div class="liquidity-text-item">
+                <div class="display-flex box-center-Y">
+                  <div class="icon-img"><img src="../../assets/image/logo@2x.png" alt=""></div>
+                  <div>Remind</div>
+                </div>
+                <div class="text-desc">Click the"Deposit button and 1 WBTC will be sent from your Ethereum address to the liquidity pool contract After the transaction is confirmed by miners, you will</div>
+              </div>
+            </div>
+            <div class="box-flex1">
+              <div class="input-item">
+                <div class="display-flex box-center-Y input-item-title">
+                  <div class="input-item-title-icon"><img src="../../assets/image/logo@2x.png" alt=""></div>
+                  <div class="box-flex1">You are buying this amount of HEGIC</div>
+                </div>
+                <div class="display-flex box-center-Y currency-input">
+                  <y-number-input :point="18" ></y-number-input>
+                  <div class="currency">HEIGC</div>
+                </div>
+              </div>
+              <div class="input-item">
+                <div class="display-flex box-center-Y input-item-title">
+                  <div class="input-item-title-icon"><img src="../../assets/image/logo@2x.png" alt=""></div>
+                  <div class="box-flex1">You are sending this amount of ETH</div>
+                </div>
+                <div class="display-flex box-center-Y currency-input">
+                  <y-number-input :point="18" ></y-number-input>
+                  <div class="currency">ETH</div>
+                </div>
+              </div>
+              <div class="btn-liquidity-deposit no-select">BUY</div>
+            </div>
+          </div>
+        </div>
+        <div class="token-box" v-show="interfaceTab.list[interfaceTab.index].type === 'token'">
+          <div class="display-flex box-center-Y">
+            <div class="box-flex1">
+              <div class="token-title">Total Staked</div>
+              <div class="number-box display-flex box-center-end">
+                <div>122131</div>
+                <div class="currency-b">HEGIC</div>
+              </div>
+            </div>
+            <div class="box-flex1">
+              <div class="token-title">Active Lots</div>
+              <div class="number-box display-flex box-center-end">
+                <div>122131</div>
+              </div>
+            </div>
+            <div class="box-flex1">
+              <div class="token-title">Settlements Fee Rate</div>
+              <div class="number-box display-flex box-center-end">
+                <div>122131</div>
+                <div class="currency-b">%</div>
+              </div>
+            </div>
+          </div>
+          <div class="display-flex box-center-Y token-2">
+            <div class="box-flex1">
+              <div class="token-title">Total Settements Fees</div>
+              <div class="number-box display-flex box-center-end">
+                <div>$122131</div>
+              </div>
+            </div>
+            <div class="box-flex1">
+              <div class="token-title">Settements Fees in WBTC</div>
+              <div class="number-box display-flex box-center-end">
+                <div>122</div>
+                <div class="currency-b">WBTC</div>
+              </div>
+            </div>
+            <div class="box-flex1">
+              <div class="token-title">Settements Fees in ETH</div>
+              <div class="number-box display-flex box-center-end">
+                <div>12</div>
+                <div class="currency-b">ETH</div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- pages/reward/js/index.js
+++ pages/reward/js/index.js
@@ -4,8 +4,276 @@ export default {
     return {
       tokenContract: null,
       account: '',
-      interfaceList: {
-        list: ['LIQUIDITY<br>NDING REWARDS', 'LIQUIDITY<br>UTILIZATION REWARDS', 'STAKING EARN<br>WBTC OR ETH', 'BONDING CURVE<br>BUY OR SELL HEGIC', 'HEGIC TOKEN<br>ANALYTICS'],
+      interfaceTab: {
+        list: [{
+          name: 'LIQUIDITY<br>ENDING REWARDS',
+          type: 'ending'
+        }, {
+          name: 'LIQUIDITY<br>UTILIZATION REWARDS',
+          type: 'utilization'
+        }, {
+          name: 'STAKING EARN<br>WBTC OR ETH',
+          type: 'stakeEarn'
+        }, {
+          name: 'BONDING CURVE<br>BUY OR SELL HEGIC',
+          type: 'bonding'
+        }, {
+          name: 'HEGIC TOKEN<br>ANALYTICS',
+          type: 'token'
+        }],
+        index: 0
+      },
+      endTab: {
+        list: [
+          {
+            currency: 'WBTC'
+          },
+          {
+            currency: 'ETH'
+          }
+        ],
+        index: 0
+      },
+      endTotalList: [
+        {
+          icon_url: require('../../../assets/image/icon_reward_end_1@2x.png'),
+          balance: 0,
+          type: 'claim',
+          currency: 'RHEGIC2',
+          btn_text: 'CLAIM REWARDS',
+          title: 'ETH POOL LIQUIDITY MINING REWARDS',
+          sub_title: 'Click the Claim Rewards button and confirm the transaction forreceiving ta be able to withdraw'
+        },
+        {
+          icon_url: require('../../../assets/image/icon_reward_end_2@2x.png'),
+          balance: 0,
+          type: 'lock',
+          currency: 'ETH',
+          btn_text: 'LOCK TOKENS',
+          title: 'Your Address Balance',
+          sub_title: 'Lock wniteETH to start receiving. You will need to manually claim HEGIC'
+        },
+        {
+          icon_url: require('../../../assets/image/icon_reward_end_3@2x.png'),
+          balance: 0,
+          type: 'unlock',
+          currency: 'ETH',
+          btn_text: 'UNLOCK TOKENS',
+          title: 'Locked',
+          sub_title: 'Unlock writrETH ta be able to withdraw awards in rHEGIC tokens your ETH from the liquidity pool'
+        }
+      ],
+      stakeEarnList: [
+        {
+          icon_url: require('../../../assets/image/icon_reward_stake_1@2x.png'),
+          balance: 0,
+          type: 'withdraw',
+          currency: 'RHEGIC2',
+          btn_text: 'WITHDRAW',
+          title: 'Total Staked',
+          sub_title: 'Click the Claim Rewards button and confirm the transaction forreceiving ta be able to withdraw'
+        },
+        {
+          icon_url: require('../../../assets/image/icon_reward_stake_2@2x.png'),
+          balance: 0,
+          type: 'stake',
+          currency: 'ETH',
+          btn_text: 'STAKE HEGIC',
+          title: 'Your Address Balance',
+          sub_title: 'Lock wniteETH to start receiving. You will need to manually claim HEGIC'
+        },
+        {
+          icon_url: require('../../../assets/image/icon_reward_stake_3@2x.png'),
+          balance: 0,
+          type: 'clalm',
+          currency: 'ETH',
+          btn_text: 'CLALM REWARDA',
+          title: 'Rewards',
+          sub_title: 'Unlock writrETH ta be able to withdraw awards in rHEGIC tokens your ETH from the liquidity pool'
+        }
+      ],
+      utilizationTab: {
+        list: [
+          {
+            currency: 'ALL'
+          },
+          {
+            currency: 'WBTC'
+          },
+          {
+            currency: 'ETH'
+          }
+        ],
+        index: 0
+      },
+      utilizationData: [
+        {
+          id: 'type',
+          type: 1231,
+          amount: 2819,
+          fee: 212,
+          reward: 12,
+          claim: 123
+        },
+        {
+          id: 'type',
+          type: 1231,
+          amount: 2819,
+          fee: 212,
+          reward: 12,
+          claim: 123
+        },
+        {
+          id: 'type',
+          type: 1231,
+          amount: 2819,
+          fee: 212,
+          reward: 12,
+          claim: 123
+        }, {
+          id: 'type',
+          type: 1231,
+          amount: 2819,
+          fee: 212,
+          reward: 12,
+          claim: 123
+        }, {
+          id: 'type',
+          type: 1231,
+          amount: 2819,
+          fee: 212,
+          reward: 12,
+          claim: 123
+        }, {
+          id: 'type',
+          type: 1231,
+          amount: 2819,
+          fee: 212,
+          reward: 12,
+          claim: 123
+        }, {
+          id: 'type',
+          type: 1231,
+          amount: 2819,
+          fee: 212,
+          reward: 12,
+          claim: 123
+        }, {
+          id: 'type',
+          type: 1231,
+          amount: 2819,
+          fee: 212,
+          reward: 12,
+          claim: 123
+        }, {
+          id: 'type',
+          type: 1231,
+          amount: 2819,
+          fee: 212,
+          reward: 12,
+          claim: 123
+        }, {
+          id: 'type',
+          type: 1231,
+          amount: 2819,
+          fee: 212,
+          reward: 12,
+          claim: 123
+        }, {
+          id: 'type',
+          type: 1231,
+          amount: 2819,
+          fee: 212,
+          reward: 12,
+          claim: 123
+        }, {
+          id: 'type',
+          type: 1231,
+          amount: 2819,
+          fee: 212,
+          reward: 12,
+          claim: 123
+        }, {
+          id: 'type',
+          type: 1231,
+          amount: 2819,
+          fee: 212,
+          reward: 12,
+          claim: 123
+        }, {
+          id: 'type',
+          type: 1231,
+          amount: 2819,
+          fee: 212,
+          reward: 12,
+          claim: 123
+        }, {
+          id: 'type',
+          type: 1231,
+          amount: 2819,
+          fee: 212,
+          reward: 12,
+          claim: 123
+        }, {
+          id: 'type',
+          type: 1231,
+          amount: 2819,
+          fee: 212,
+          reward: 12,
+          claim: 123
+        }, {
+          id: 'type',
+          type: 1231,
+          amount: 2819,
+          fee: 212,
+          reward: 12,
+          claim: 123
+        }, {
+          id: 'type',
+          type: 1231,
+          amount: 2819,
+          fee: 212,
+          reward: 12,
+          claim: 123
+        }, {
+          id: 'type',
+          type: 1231,
+          amount: 2819,
+          fee: 212,
+          reward: 12,
+          claim: 123
+        }, {
+          id: 'type',
+          type: 1231,
+          amount: 2819,
+          fee: 212,
+          reward: 12,
+          claim: 123
+        }, {
+          id: 'type',
+          type: 1231,
+          amount: 2819,
+          fee: 212,
+          reward: 12,
+          claim: 123
+        }, {
+          id: 'type',
+          type: 1231,
+          amount: 2819,
+          fee: 212,
+          reward: 12,
+          claim: 123
+        }, {
+          id: 'type',
+          type: 1231,
+          amount: 2819,
+          fee: 212,
+          reward: 12,
+          claim: 123
+        }
+      ],
+      boundTab: {
+        list: ['BUY', 'SELL'],
         index: 0
       }
     }
@@ -17,7 +285,16 @@ export default {
     setAccount () {
     },
     changeInterfaceTab (i) {
-      this.interfaceList.index = i
+      this.interfaceTab.index = i
+    },
+    changeUtilizationTab (i) {
+      this.utilizationTab.index = i
+    },
+    changeEndTab (i) {
+      this.endTab.index = i
+    },
+    changeBoundTab (i) {
+      this.boundTab.index = i
     }
   }
 }
